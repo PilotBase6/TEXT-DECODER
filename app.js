d735eea90e1aba367870a12745c1e47d01ef69ac
Andres Guevara <guevara.c0610@gmail.com>
2023-02-27 16:28:14 -0500

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const aboutNav = () => {
+    const headeraboutapp = document.getElementsByClassName(principal_header_items_link-aboutaplication);
+    headeraboutapp.addEventListener('click',);
+
+}
+
+
